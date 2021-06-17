9750395629d5d00e6d16085184a3f54283dc9697
moirahartigan <moirahartigan@gmail.com>
2021-06-17 15:04:46 +0000

    code is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,56 @@
-// scripts.js
+// define variables
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
-let firstCard, secondCard;
+let flippedCard = false; //used to check if card has already been clicked 
+let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
+let firstCard, secondCard; //Used to check for cards match
 
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+/*
+onclick function for cards, add flip class for css effects
+code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
+*/
 function flipCard() {
-this.classList.add('flip');
+if(lockBoard) return; // return is lockBoard is true so the rest of the function wont be executed
+this.classList.add('flip'); //if valid, flips card using css class
 
-if (!hasFlippedCard){
-    hasFlippedCard = true;
-    firstCard = this;
+if (!flippedCard){
+    // The first card clicked
+    flippedCard = true;
+    firstCard = this; //stores this as first card
    } else {
-       hasFlippedCard = false;
-       secondCard = this;
-
-    //  do they match? - if not thay will be flipped back
-    if (firstCard.dataset.image ===
-        secondCard.dataset.image) {
-            // if its a match the eventlistener is disabled
-        firstCard.removeEventListener('click', flipCard);  
-        secondCard.removeEventListener('click', flipCard);  
-        } else {
-            // then if its not a match the flipclass is removed
-         setTimeout(() => {
-           firstCard.classList.remove('flip');
-         secondCard.classList.remove('flip');  
-         }, 900); 
-         
-        }        
+       //The second card clicked
+       flippedCard = false;
+       secondCard = this; 
+       
+    //  Check if cards match? - if not thay will be flipped back
+  if (firstCard.dataset.image === secondCard.dataset.image) {
+    disableCards(); 
+    } else {
+    unflipCards();  
+    }     
    }
 }
 
+function disableCards(){
+// if its a match the eventlistener is disabled
+firstCard.removeEventListener('click', flipCard);  
+secondCard.removeEventListener('click', flipCard); 
+}
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+function unflipCards(){
+    lockBoard = true; // if no match, board is locked until cards are flipped back
+ // then if its not a match the flipclass is removed
+ setTimeout(() => {
+    firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');
+  
+  lockBoard = false;
+  }, 900);  
+  
+}
+
+
+
