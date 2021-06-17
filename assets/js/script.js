68ef6fe1583fc13b942c95c868e8649f019a7be7
moirahartigan <moirahartigan@gmail.com>
2021-06-17 19:56:07 +0000

    Add reset function that works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // define variables
 const cards = document.querySelectorAll('.memory-card');
 
+
 let flippedCard = false; //used to check if card has already been clicked 
 let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
 let firstCard, secondCard; //Used to check for cards match
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
+shuffle();
 
 /*
 onclick function for cards, add flip class for css effects
@@ -14,6 +15,7 @@ code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and 
 */
 function flipCard() {
 if(lockBoard) return; // return is lockBoard is true so the rest of the function wont be executed
+if (this === firstCard) return;
 this.classList.add('flip'); //if valid, flips card using css class
 
 if (!flippedCard){
@@ -38,6 +40,8 @@ function disableCards(){
 // if its a match the eventlistener is disabled
 firstCard.removeEventListener('click', flipCard);  
 secondCard.removeEventListener('click', flipCard); 
+
+resetBoard();
 }
 
 function unflipCards(){
@@ -47,10 +51,70 @@ function unflipCards(){
     firstCard.classList.remove('flip');
   secondCard.classList.remove('flip');
   
-  lockBoard = false;
+  resetBoard();
   }, 900);  
   
 }
 
+function resetBoard(){
+  [flippedCard, lockBoard] = [false, false];
+  [firstCard,secondCard] = [null, null];
+}
 
+// Card shuffle
+function shuffle() {
+  cards.forEach(cards => {
+      let randomPosition = Math.floor(Math.random() * 16);
+      cards.style.order = randomPosition;
+  })
+
+};
+
+// game info -timer 
+
+
+let moveCounter = 0;
+let timer = {
+  seconds: 0,
+  minutes: 0,
+  clearTime: -1
+};
+
+//Start time first card is clicked
+
+//Start timer
+let startTimer = function() {
+  if (timer.seconds === 59) {
+    timer.minutes++;
+    timer.seconds = 0;
+  } else {
+    timer.seconds++;
+  };
+  // Ensure that single digit seconds are preceded with a 0
+  var formattedSec = "0";
+  if (timer.seconds < 10) {
+    formattedSec += timer.seconds;
+  } else {
+    formattedSec = String(timer.seconds);
+  }
+
+  var time = String(timer.minutes) + ":" + formattedSec;
+  $(".timer").text(time);
+};
+
+
+function reset(){
+  setTimeout(() => {
+    flippedCard = false;
+    [firstCard, secondCard] = [null, null];
+    pairs = 8;
+    timer = 0;
+    moves = 0;
+    cards.forEach(cardReset => cardReset.classList.remove('flip'));
+    shuffle();
+    cards.forEach(card => card.addEventListener('click', flipCard));
+  }, 500);
+  console.log("reset button clicked");
+}
+   
 
