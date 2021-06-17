6a117660d0dd7cefa245df7857b00b07388b53ed
moirahartigan <moirahartigan@gmail.com>
2021-06-17 13:20:52 +0000

    Add flip card function and match card - before refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,35 @@
-// Wait for the Dom to finish loading before running the game
-// get button elements and add event listeners to them
-
+// scripts.js
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false; //used to check if card has already been clicked 
-let lockBoard = false;
-let firstCard, secondCard; //Used to check for cards match
-/*
-onclick function for cards, add flip class for css effects
-code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
-*/
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+this.classList.add('flip');
 
-    this.classList.add('flip'); //if valid, flips card using css class
+if (!hasFlippedCard){
+    hasFlippedCard = true;
+    firstCard = this;
+   } else {
+       hasFlippedCard = false;
+       secondCard = this;
 
-    if (!hasFlippedCard) {
-        // first click
-        hasFlippedCard = true;
-        firstCard = this; //stores this as first card
-        return;
-    } 
-        //second card
-        secondCard = this;
-        
-        checkForMatch();
-    }
+    //  do they match? - if not thay will be flipped back
+    if (firstCard.dataset.image ===
+        secondCard.dataset.image) {
+            // if its a match the eventlistener is disabled
+        firstCard.removeEventListener('click', flipCard);  
+        secondCard.removeEventListener('click', flipCard);  
+        } else {
+            // then if its not a match the flipclass is removed
+         setTimeout(() => {
+           firstCard.classList.remove('flip');
+         secondCard.classList.remove('flip');  
+         }, 900); 
+         
+        }        
+   }
+}
 
-/*
-ternary operator checks if firstCard & secondCard 'data-image' are a match
-code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
-*/
-function checkForMatch() {
-    let isMatch = firstCard.dataset.image === secondCard.dataset.image; //checks if dataset.image are a match
-      isMatch ? disableCards() : unflipCards();
-      }
-        
-     
-function disableCards() {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-          
-        resetGameBoard();
-        }
-          
-function unflipCards() {
-        lockBoard = true;
-          
-        setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-          
-        resetGameBoard();
-        lockBoard = false;
-        }, 700);
-          }
- /*
-resetBoardStatus clears values used in flipcard & checkformatch functions
-code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
-*/         
-function resetGameBoard() {
-            [hasFlippedCard, lockBoard] = [false, false];
-            [firstCard, secondCard] = [null, null];
-          }
-          
-(function shuffle() {
-            cards.forEach(card => {
-               let randomPos = Math.floor(Math.random() * 16);
-              card.style.order = randomPos;
-             });
-          })();
-  
- // let shuffleCard = [monster-1, monster-1, monster-2, monster-2, monster-3, monster-3, monster-4, monster-4, 
-   // monster-5, monster-5, monster-6, monster-6, monster-7, monster-7, monster-8, monster-8];
 
-    //function shuffleImages() {
-    //for (let i = 0; i < arr.length; i++)
-    //shuffleCard.push(arr[i], arr[i]);
- // }       
-          
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-// game timer
-
-    let secs = 0
-    let mins = 0
-    let SS
-    let MM
-    let timer = document.querySelectorAll("#time-remaining")
-function startTimer() {    
-    setInterval(()=>{
-        secs++
-        if(secs==60){
-            secs=0; 
-            mins++
-        }
-
-        secs<10?SS=`0${secs}`:SS=`${secs}`
-        mins<10?MM=`0${mins}`:SS=`${mins}`
-        
-        document.querySelector('#time-remaining').innerHTML = `${MM}:${SS}`
-    }, 1000)
-
-}
-
-function gameOverMsg() {
-
-}
- 
