7f5dafdb255e52240cdaa675d135f4f8cc89b784
moirahartigan <moirahartigan@gmail.com>
2021-06-18 19:23:40 +0000

    end of the day still working on timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const cards = document.querySelectorAll('.memory-card');
 let flippedCard = false; //used to check if card has already been clicked 
 let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
 let firstCard, secondCard; //Used to check for cards match
+let firstClick = 0;
+let time = 0;
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 shuffle();
@@ -14,37 +16,58 @@ onclick function for cards, add flip class for css effects
 code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */
 function flipCard() {
+  firstClick += 1;
+  if(timer.seconds == 0 && timer.minutes == 0){
+    timer();
+  } // starts timer on firstClick
+
 if(lockBoard) return; // return is lockBoard is true so the rest of the function wont be executed
 if (this === firstCard) return;
 this.classList.add('flip'); //if valid, flips card using css class
-
+let len = moves.length;
+if (len === 2){
+  moveCounter++;
+  moveCounter();
+}
 if (!flippedCard){
     // The first card clicked
     flippedCard = true;
     firstCard = this; //stores this as first card
-   } else {
-       //The second card clicked
-       flippedCard = false;
-       secondCard = this; 
-       
-    //  Check if cards match? - if not thay will be flipped back
-  if (firstCard.dataset.image === secondCard.dataset.image) {
-    disableCards(); 
-    } else {
-    unflipCards();  
-    }     
-   }
+    timer();
+    return;
+    
 }
 
-function disableCards(){
+//The second card clicked
+flippedCard = false;
+secondCard = this; 
+
+moveCounter();
+checkCardMatch();
+}
+
+/*
+ternary operator checking if firstCard & secondCard 'data-id' are a match
+initial code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
+*/
+function checkCardMatch() {
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+
+  isMatch ? pairMatch() : noMatch();
+ }
+       
+  
+
+function pairMatch(){
 // if its a match the eventlistener is disabled
 firstCard.removeEventListener('click', flipCard);  
 secondCard.removeEventListener('click', flipCard); 
 
-resetBoard();
+showWinMessage();
+resetBoard();  
 }
 
-function unflipCards(){
+function noMatch(){
     lockBoard = true; // if no match, board is locked until cards are flipped back
  // then if its not a match the flipclass is removed
  setTimeout(() => {
@@ -56,10 +79,13 @@ function unflipCards(){
   
 }
 
+
+
 function resetBoard(){
   [flippedCard, lockBoard] = [false, false];
   [firstCard,secondCard] = [null, null];
 }
+
 
 // Card shuffle
 function shuffle() {
@@ -70,19 +96,53 @@ function shuffle() {
 
 };
 
+
+// move counter & game timer 
+let moves = 0;
+function moveCounter() {
+  moves++;
+ 
+}
+
+//game timer -https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+let sec = 0;
+function timer(val) {
+  return  val > 9 ? val : "0" + val;
+}
+setInterval(function(){
+  document.getElementById("seconds").innerHTML=timer(++sec%60);
+  document.getElementById("minutes").innerHTML=timer(parseInt(sec/60,10));
+}, 1000);
+  
+
 function reset(){
   setTimeout(() => {
     flippedCard = false;
     [firstCard, secondCard] = [null, null];
-    pairs = 8;
+    
     timer = 0;
     moves = 0;
     cards.forEach(cardReset => cardReset.classList.remove('flip'));
     shuffle();
     cards.forEach(card => card.addEventListener('click', flipCard));
   }, 500);
-  console.log("reset button clicked"); // 
+  timer(); // timer reset will go here
 }
 
+const winMsg = document.getElementById('win-msg');
+const modal = document.getElementById('modal');
+const close = document.getElementById('close');
 
+//add an event for when all pairs match and game is won 
+//if (pairMatch.length === 16){
+ // showWinMessage();
+//}
+
+close.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
+
+function showWinMessage(){
+  
+}
 
