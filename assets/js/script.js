f1065b5a9382ba6dd6baa289400a349e4de1ff7c
moirahartigan <moirahartigan@gmail.com>
2021-06-20 10:05:18 +0000

    add move counter function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // define variables
 const cards = document.querySelectorAll('.memory-card');
 
-
+//on first flipped card if match with second flipped card it will lock both cards
 let flippedCard = false; //used to check if card has already been clicked 
 let lockBoard = false; // used to lock the board until each set of cards are finished are finished before selecting the next two
 let firstCard, secondCard; //Used to check for cards match
-let firstClick = 0;
-let time = 0;
+let moves = 0;
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 shuffle();
@@ -16,38 +16,27 @@ onclick function for cards, add flip class for css effects
 code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */
 function flipCard() {
-  firstClick += 1;
-  if(timer.seconds == 0 && timer.minutes == 0){
-    timer();
-  } // starts timer on firstClick
-
 if(lockBoard) return; // return is lockBoard is true so the rest of the function wont be executed
 if (this === firstCard) return;
+
 this.classList.add('flip'); //if valid, flips card using css class
-let len = moves.length;
-if (len === 2){
-  moveCounter++;
-  moveCounter();
-}
+
 if (!flippedCard){
     // The first card clicked
     flippedCard = true;
     firstCard = this; //stores this as first card
-    timer();
-    return;
-    
+  
+    return;    
 }
 
 //The second card clicked
-flippedCard = false;
 secondCard = this; 
 
-moveCounter();
 checkCardMatch();
 }
 
 /*
-ternary operator checking if firstCard & secondCard 'data-id' are a match
+ternary operator checking if firstCard & secondCard 'data-id' are a match and if so prevents them flipping back
 initial code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted
 */
 function checkCardMatch() {
@@ -57,9 +46,10 @@ function checkCardMatch() {
  }
        
   
-
+// matched cards will be disabled for clicks once they are flipped
 function pairMatch(){
 // if its a match the eventlistener is disabled
+
 firstCard.removeEventListener('click', flipCard);  
 secondCard.removeEventListener('click', flipCard); 
 
@@ -67,41 +57,30 @@ showWinMessage();
 resetBoard();  
 }
 
+// if no match, board is locked until cards are flipped back
 function noMatch(){
-    lockBoard = true; // if no match, board is locked until cards are flipped back
- // then if its not a match the flipclass is removed
+  lockBoard = true; 
+ 
  setTimeout(() => {
     firstCard.classList.remove('flip');
   secondCard.classList.remove('flip');
   
   resetBoard();
   }, 900);  
-  
+
+  // Add move
+addMove();
+
 }
-
-
-
-function resetBoard(){
-  [flippedCard, lockBoard] = [false, false];
-  [firstCard,secondCard] = [null, null];
-}
-
-
-// Card shuffle
-function shuffle() {
-  cards.forEach(cards => {
-      let randomPosition = Math.floor(Math.random() * 16);
-      cards.style.order = randomPosition;
-  })
-
-};
-
 
 // move counter & game timer 
-let moves = 0;
-function moveCounter() {
+const moveContainer = document.querySelector(".moves");
+moves = 0;
+moveContainer.innerHtml = 0;
+function addMove() {
   moves++;
- 
+  moveContainer.innerHTML = moves;
+ console.log('function selected');
 }
 
 //game timer -https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
@@ -145,4 +124,24 @@ close.addEventListener('click', () => {
 function showWinMessage(){
   
 }
+
+
+
+function resetBoard(){
+  [flippedCard, lockBoard] = [false, false];
+  [firstCard,secondCard] = [null, null];
+}
+
+
+// Card shuffle
+function shuffle() {
+  cards.forEach(cards => {
+      let randomPosition = Math.floor(Math.random() * 16);
+      cards.style.order = randomPosition;
+  })
+
+};
+
+
+
 
